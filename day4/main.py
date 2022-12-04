b1a342af66e5123e42e428bec65e8fa16f703fc8
Carter Mintey <cmintey8@gmail.com>
2022-12-04 11:48:19 -0600

    added second solution for day 4 using comparisons

--- day4/main.py
+++ day4/main.py
@@ -12,12 +12,9 @@ def parse_input(filename: str) -> list[str]:
 def part_1(input: list[str]) -> int:
     c = 0
     for pair in input:
-        r1, r2 = list(map(lambda x: x.split("-"), pair.split(",")))
-        s1, s2 = set(range(int(r1[0]), int(r1[1]) + 1)), set(
-            range(int(r2[0]), int(r2[1]) + 1)
-        )
-        inter = s1 & s2
-        if s1 == inter or s2 == inter:
+        ranges = map(lambda x: x.split("-"), pair.split(","))
+        s1, s2 = [set(range(int(x[0]), int(x[1]) + 1)) for x in ranges]
+        if s1.issubset(s2) or s2.issubset(s1):
             c += 1
     return c
 
@@ -25,16 +22,54 @@ def part_1(input: list[str]) -> int:
 def part_2(input: list[str]) -> int:
     c = 0
     for pair in input:
-        r1, r2 = list(map(lambda x: x.split("-"), pair.split(",")))
-        s1, s2 = set(range(int(r1[0]), int(r1[1]) + 1)), set(
-            range(int(r2[0]), int(r2[1]) + 1)
-        )
+        ranges = map(lambda x: x.split("-"), pair.split(","))
+        s1, s2 = [set(range(int(x[0]), int(x[1]) + 1)) for x in ranges]
         inter = s1 & s2
         if inter:
             c += 1
     return c
 
 
+def part_1_compare(input: list[str]) -> int:
+    y = 0
+    for pair in input:
+        ranges = map(lambda x: x.split("-"), pair.split(","))
+        (a, b), (c, d) = [(int(x[0]), int(x[1])) for x in ranges]
+        """
+              a ----- b
+            c ---------- d
+        ---------------------
+            a --------- b
+               c --- d
+        """
+        if a <= c and b >= d or c <= a and d >= b:
+            y += 1
+    return y
+
+
+def part_2_compare(input: list[str]) -> int:
+    y = 0
+    for pair in input:
+        ranges = map(lambda x: x.split("-"), pair.split(","))
+        (a, b), (c, d) = [(int(x[0]), int(x[1])) for x in ranges]
+        """
+              a ----- b
+            c --- d
+        ---------------------
+            a - b
+               c --- d
+        """
+        if a <= c <= b or c <= a <= d or a <= d <= b or c <= b <= d:
+            y += 1
+    return y
+
+
 input = parse_input("input.txt")
+
+# initial solution using sets
 print(part_1(input))
 print(part_2(input))
+
+# second solution doing direct comparison
+print(part_1_compare(input))
+print(part_2_compare(input))
