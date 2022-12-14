19fdb2c25660c302e989270aacfc6901e036057d
Carter Mintey <cmintey8@gmail.com>
2022-12-14 10:25:59 -0600

    day 14 complete

--- day14/main.py
+++ day14/main.py
@@ -1,110 +1,94 @@
-from functools import cmp_to_key
+from collections import defaultdict
 import os
 
 input_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "input/")
 
 
-def parse_input(filename: str) -> list[list[str]]:
+def parse_input(filename: str) -> defaultdict[tuple[int, int], int]:
     filepath = os.path.join(input_dir, filename)
     lines = open(filepath).read().strip().split("\n")
-    rocks = set()
+    rocks = defaultdict(lambda: 0)
     for path in lines:
         coords = path.strip().split(" -> ")
         for i in range(1, len(coords)):
             [x1, y1] = map(int, coords[i - 1].split(","))
             [x2, y2] = map(int, coords[i].split(","))
             if x1 < x2:
-                rocks.update(zip(range(x1, x2 + 1), [y1] * (1 + x2 - x1)))
+                for x in range(x1, x2 + 1):
+                    rocks[x, y1] = 1
             if x1 > x2:
-                rocks.update(zip(range(x2, x1 + 1), [y1] * (1 + x1 - x2)))
+                for x in range(x2, x1 + 1):
+                    rocks[x, y1] = 1
             if y1 < y2:
-                rocks.update(zip([x1] * (1 + y2 - y1), range(y1, y2 + 1)))
+                for y in range(y1, y2 + 1):
+                    rocks[x1, y] = 1
             if y1 > y2:
-                rocks.update(zip([x1] * (1 + y1 - y2), range(y2, y1 + 1)))
+                for y in range(y2, y1 + 1):
+                    rocks[x1, y] = 1
 
     return rocks
 
 
-def display(rocks, sand_spaces):
-    max_x = max(rocks | sand_spaces, key=lambda x: x[0])[0] + 1
-    min_x = min(rocks | sand_spaces, key=lambda x: x[0])[0] - 1
-    max_y = max(rocks, key=lambda x: x[1])[1] + 2
+def display(grid: defaultdict[tuple[int, int], int]):
+    max_x = max(grid.keys(), key=lambda x: x[0])[0] + 1
+    min_x = min(grid.keys(), key=lambda x: x[0])[0] - 1
+    max_y = max(grid.keys(), key=lambda x: x[1])[1] + 2
     for y in range(0, max_y):
         for x in range(min_x, max_x + 1):
-            if (x, y) in rocks:
+            if grid[(x, y)] == 1:
                 print("#", end="")
-            elif (x, y) in sand_spaces:
+            elif grid[(x, y)] == 2:
                 print("o", end="")
             else:
                 print(".", end="")
         print()
 
 
-def part_1(rocks: set[tuple[int, int]]) -> int:
-    void_y = max(rocks, key=lambda x: x[1])[1] + 1
-    space_available = True
-    sand_spaces = set()
-    sand = None
-    while space_available:
-        if not sand:
-            sand = (500, 1)
-
+def part_1(cave: defaultdict[tuple[int, int], int]) -> int:
+    void_y = max(cave.keys(), key=lambda x: x[1])[1] + 1
+    sand = (500, 0)
+    while True:
         if sand[1] >= void_y:
             break
 
-        if (pos := (sand[0], sand[1] + 1)) not in rocks | sand_spaces:
-            sand = pos
-        elif (pos := (sand[0] - 1, sand[1] + 1)) not in rocks | sand_spaces:
-            sand = pos
-        elif (pos := (sand[0] + 1, sand[1] + 1)) not in rocks | sand_spaces:
-            sand = pos
-        else:
-            sand_spaces.add(sand)
-            sand = None
-    display(rocks, sand_spaces)
-    return len(sand_spaces)
-
-
-def part_2(rocks: set[tuple[int, int]]) -> int:
-    # max_x = max(rocks, key=lambda x: x[0])[0] + 1000
-    # min_x = min(rocks, key=lambda x: x[0])[0] - 100
-    max_y = max(rocks, key=lambda x: x[1])[1] + 2
-    # rocks.update(zip(range(min_x, max_x), [max_y] * (1 + max_x - min_x)))
-
-    sand_spaces = set()
-    sand = None
-    i = 0
-    while True:
-        if (i % 1000) == 0:
-            print(f"{i} steps taken")
-
-        if not sand:
+        blocked = True
+        for dx in [0, -1, 1]:
+            if cave[(pos := (sand[0] + dx, sand[1] + 1))] == 0:
+                sand = pos
+                blocked = False
+                break
+        if blocked:
+            cave[sand] = 2
             sand = (500, 0)
 
-        if sand[1] + 1 == max_y:
-            sand_spaces.add(sand)
-            sand = None
-            continue
+    return sum([1 for v in cave.values() if v == 2])
 
-        if (pos := (sand[0], sand[1] + 1)) not in rocks | sand_spaces:
-            sand = pos
-        elif (pos := (sand[0] - 1, sand[1] + 1)) not in rocks | sand_spaces:
-            sand = pos
-        elif (pos := (sand[0] + 1, sand[1] + 1)) not in rocks | sand_spaces:
-            sand = pos
-        else:
-            sand_spaces.add(sand)
-            sand = None
 
-        if (500, 0) in sand_spaces:
+def part_2(rocks: defaultdict[tuple[int, int], int]) -> int:
+    max_y = max(rocks.keys(), key=lambda x: x[1])[1] + 2
+
+    sand = (500, 0)
+    while True:
+        if rocks[(500, 0)] == 2:
             break
 
-        i += 1
-    display(rocks, sand_spaces)
-    return len(sand_spaces)
+        blocked = True
+        for dx in [0, -1, 1]:
+            if sand[1] + 1 == max_y:
+                blocked = True
+            elif rocks[(pos := (sand[0] + dx, sand[1] + 1))] == 0:
+                sand = pos
+                blocked = False
+
+        if blocked:
+            rocks[sand] = 2
+            sand = (500, 0)
+
+    return sum([1 for v in rocks.values() if v == 2])
 
 
 input = parse_input("input.txt")
 print(part_1(input))
-print()
+
+input = parse_input("input.txt")
 print(part_2(input))
