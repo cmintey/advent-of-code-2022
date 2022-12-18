aa8c563bd618572bce9b626eac48ee681e180d5b
Carter Mintey <cmintey8@gmail.com>
2022-12-18 11:02:38 -0600

    day 17 part 2

--- day17/main.py
+++ day17/main.py
@@ -1,6 +1,4 @@
 import os
-import re
-from tqdm import tqdm
 
 input_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "input/")
 
@@ -49,106 +47,82 @@ def display(rocks, height):
     print("+-------+")
 
 
-DEBUG = False
-
 # |0123456| 2
 # |.......| 1
 # +-------+
-def part_1(jet_pattern: list[str]) -> int:
+def simulate(jet_pattern: list[str], n_rocks: int):
     cave_width = 7
     cave_floor = 1
     height = 0
     rocks = set()
+    seen = {}
     jet, n_jets = 0, len(jet_pattern)
-    for i in range(2022):
-        if DEBUG:
-            print("rock", i)
+    i = 0
+    while i < n_rocks:
         rock = SHAPES[i % 5]
         y_pos = cave_floor + height + 3
         x_pos = 3 if (i % 5) == 1 else 2
+
+        # logic courtesy of https://github.com/terminalmage/adventofcode/blob/main/2022/day17.py
+        key = (i % 5, jet)
+        if key in seen:
+            seen_height, seen_rock_num = seen[key]
+            cycle_length = i - seen_rock_num
+            if i % cycle_length == n_rocks % cycle_length:
+                cycle_height = height - seen_height
+                rocks_remain = n_rocks - i
+                cycles_remain = (rocks_remain // cycle_length) + 1
+                return seen_height + (cycle_height * cycles_remain)
+        else:
+            seen[key] = (height, i)
+
         falling = True
         while falling:
-            if DEBUG:
-                print("x, y", x_pos, y_pos)
             # jet motion
             motion = jet_pattern[jet]
             jet = (jet + 1) % n_jets
             if motion == "<":
-                if DEBUG:
-                    print("try left")
                 left_move = rock[2]((x_pos - 1, y_pos))
-                if DEBUG:
-                    print(left_move)
-                # if x_pos - 1 >= 0:
+                # check for touching wall
                 if all([x[0] >= 0 for x in left_move]):
-                    if left_move & rocks:
-                        if DEBUG:
-                            print("  blocked by rock")
-                    else:
-                        if DEBUG:
-                            print(" success")
+                    # ceck for touching rock
+                    if not left_move & rocks:
                         x_pos -= 1
             if motion == ">":
-                if DEBUG:
-                    print(f"try right")
                 right_move = rock[2]((x_pos + 1, y_pos))
-                if DEBUG:
-                    print(right_move)
+                # check for touching wall
                 if all([x[0] < cave_width for x in right_move]):
-                    if right_move & rocks:
-                        if DEBUG:
-                            print("  blocked by rock")
-                    else:
-                        if DEBUG:
-                            print("  success")
+                    # check for touching rock
+                    if not right_move & rocks:
                         x_pos += 1
-            if DEBUG:
-                print("x", x_pos)
 
-            if DEBUG:
-                print("go down")
             # drop one
             if y_pos - 1 >= cave_floor:
-                # if touching another rock
+                # check for touching another rock
                 if rock[2]((x_pos, y_pos - 1)) & rocks:
                     new_rocks = rock[2]((x_pos, y_pos))
                     rocks = rocks | new_rocks
                     height = max(height, max(new_rocks, key=lambda x: x[1])[1])
-                    if DEBUG:
-                        print("  blocked by rock")
                     falling = False
-                # rocks.append; break;
                 else:
-                    if DEBUG:
-                        print("  success")
                     y_pos -= 1
             else:
-                if DEBUG:
-                    print("  hit ground")
-                # rocks.append()
                 new_rocks = rock[2]((x_pos, y_pos))
                 rocks = rocks | new_rocks
                 height = max(height, max(new_rocks, key=lambda x: x[1])[1])
                 falling = False
-            if DEBUG:
-                print("x, y", x_pos, y_pos)
-        if DEBUG:
-            print(rocks)
-        if DEBUG:
-            display(rocks, height)
-        if DEBUG:
-            print(height)
-        if DEBUG:
-            print()
-    # display(rocks, height)
-    # print(rocks)
+        i += 1
     return height
 
 
-def part_2(sensor_map: dict[tuple[int, int], tuple[int, int]]) -> int:
-    pass
+def part_1(jet_pattern: list[str]) -> int:
+    return simulate(jet_pattern, 2022)
+
+
+def part_2(jet_pattern: list[str]) -> int:
+    return simulate(jet_pattern, 1000000000000)
 
 
 input = parse_input("input.txt")
 print(part_1(input))
-# print(part_2(input))
+print(part_2(input))
